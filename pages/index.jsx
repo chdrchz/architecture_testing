f4e9df8ca9318ec78eb72199b57e2803cf3f7c67
chdrchz <saavannaa.rose@gmail.com>
2024-10-24 20:06:20 -0500

    adding a pages folder to build

---
+++ pages/index.jsx
@@ -0,0 +1,13 @@
+// pages/index.js
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to My Next.js App!</h1>
+      <p>This is the home page of your application.</p>
+    </div>
+  );
+};
+
+export default Home;
